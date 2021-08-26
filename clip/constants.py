443a7ff00451023503b42a6d1ef3b2c521fad578
Shahbuland Matiana <ssmatian@uwaterloo.ca>
2021-08-26 16:20:30 -0400

    Preparing to train NEO

--- clip/constants.py
+++ clip/constants.py
@@ -1,7 +1,7 @@
 N_CTX = 512
 EPOCHS = 10
-BATCH_SIZE = 1024 # Contrastive batch
-MICROBATCH_SIZE = 8 # Minibatches in contrastive batch
+BATCH_SIZE = 2040 # Contrastive batch
+MICROBATCH_SIZE = 14 # Minibatches in contrastive batch
 
 # Size of encodings
 LATENT_DIM = 2048
@@ -9,7 +9,7 @@ PROJ_DROPOUT = 0.1
 LINEAR_PROJECTION = True # Uses more complex projection head if false
 
 # info on HF model being used
-MODEL_PATH = "microsoft/deberta-v2-xlarge"
+MODEL_PATH = "EleutherAI/gpt-neo-2.7B"
 
 # training
 LR_RAMP_STEPS = 400
@@ -19,12 +19,12 @@ LEARNING_RATE_TARGET = 1e-6
 
 GRAD_CLIP = 1.0 # What to clip grad norms to (set to -1 for no clip)
 
-LOG_INTERVAL = 1
+LOG_INTERVAL = 2
 CHECKPOINT_INTERVAL = 15
 VALIDATE_INTERVAL = 50
 LOAD_CHECKPOINT = False
 USE_HALF = False
-DO_LOG = True # Log to WANDB?
+DO_LOG = False # Log to WANDB?
 # For dataset
 VALIDATION_SIZE = 1000
 USE_BUCKET = False
